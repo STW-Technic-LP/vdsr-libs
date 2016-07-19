878f0d1da4f50b41c140a436f4299e00abb6dd80
stw <brett.berry@stw-technic.com>
2016-07-19 10:44:31 -0400

    add more canopen capabilities

--- examples/edsTest.js
+++ examples/edsTest.js
@@ -1,7 +1,22 @@
 var edsParser = require('../libs/edsParser.js');
 var fs = require('fs');
+var path = require('path');
 
-fs.readFile('./wachendorffBad.eds', {encoding: 'utf8'}, function(err, f){
+var filename = '';
+if(process.argv[2] && process.argv[2].length){
+   filename = process.argv[2];
+}
+
+if(!filename.length){
+   console.log('Must pass in a filename as an argument');
+   process.exit(1);
+}
+
+fs.readFile(path.join('./', filename), {encoding: 'utf8'}, function(err, f){
+   if(err){
+      console.log(err);
+      process.exit(1);
+   }
    var parsed = edsParser(f);
 
    console.log(JSON.stringify(parsed));
