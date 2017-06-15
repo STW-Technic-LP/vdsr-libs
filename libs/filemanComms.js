8dbb410c7471c27564ab872764b32cad98545adc
stw <brett.berry@stw-technic.com>
2017-06-15 09:24:32 -0400

    Fix filemanComms protocol

--- libs/filemanComms.js
+++ libs/filemanComms.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var comms;
-
+var Bluebird = require('bluebird');
 var channelName = 'fileMan';
+var requestCounter = 0;
 
 module.exports = function(opts){
 
@@ -17,38 +18,78 @@ module.exports = function(opts){
 };
 
 function list(device, opts){
-   if(!_isObject(opts)){
-      opts = {};
+   if(!_isObject(opts) || !_isObject(opts.options) || !opts.options.hasOwnProperty('interfaceType')){
+      return Bluebird.reject('No interface configured for this device: '+device);
    }
-   opts.device = device;
-   comms.send(channelName, 'list', opts);
+
+   return _request(channelName, 'list', {
+      serialNumber: device,
+      commandOptions: opts.options
+   })
+   .then(function(ret){
+      if(ret.error){
+         var str = "Error retrieving list of files: " + ret.error.message;
+         return Bluebird.reject(new Error(str));
+      }
+      return ret;
+   });
 }
 
-function destroy(files, opts){
-   if(!_isObject(opts)){
-      opts = {};
+function destroy(device, files, opts){
+   if(!_isObject(opts) || !_isObject(opts.options) || !opts.options.hasOwnProperty('interfaceType')){
+      return Bluebird.reject('No interface configured for this device: '+device);
    }
-   opts.files = files;
-   comms.send(channelName, 'destroy', opts);
+   opts.options.filenames = files;
+
+   return _request(channelName, 'destroy', {
+      serialNumber: device,
+      commandOptions: opts.options
+   });
 }
 
-function create(files, opts){
-   if(!_isObject(opts)){
-      opts = {};
+function create(device, file, opts){
+   if(!_isObject(opts) || !_isObject(opts.options) || !opts.options.hasOwnProperty('interfaceType')){
+      return Bluebird.reject('No interface configured for this device: '+device);
    }
-   opts.files = files;
-   comms.send(channelName, 'create', opts);
+
+   opts.commandOptions = Object.assign(opts, file);
+
+   return _request(channelName, 'create', {
+      serialNumber: device,
+      commandOptions: opts.options
+   });
 }
 
-function read(device, files, opts){
-   if(!_isObject(opts)){
-      opts = {};
+function read(device, filename, opts){
+   if(!_isObject(opts) || !_isObject(opts.options) || !opts.options.hasOwnProperty('interfaceType')){
+      return Bluebird.reject('No interface configured for this device: '+device);
    }
-   opts.files = files;
-   opts.device = device;
-   comms.send(channelName, 'read', opts);
+
+   opts.filename = filename;
+   return _request(channelName, 'read', {
+      serialNumber: device,
+      commandOptions: opts.options
+   });
 }
 
 function _isObject(o){
    return typeof o === 'object' && o !== null && o !== undefined;
+}
+
+function _request(channel, cmd, data, noResponse){
+   requestCounter++;
+   return new Bluebird(function(resolve, reject){
+      data.reqId = requestCounter;
+      comms.send(channel, cmd, data);
+      if(noResponse){
+         resolve();
+      }
+      var timeoutId = setTimeout(function(){
+         reject(new Error('Request timeout for command: '+cmd));
+      }, 30000);
+      comms.once(cmd+requestCounter, function(d){
+         clearTimeout(timeoutId);
+         resolve(d);
+      });
+   });
 }
