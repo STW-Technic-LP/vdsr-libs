bf8ddb1ace1f85fb480d0607fc9d25bf0deb4494
Nihat Özer <nihatforweb@gmail.com>
2021-03-29 15:10:30 +0300

    fix typo

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const redisService = require('./libs/redisService.js');
 const fsService = require('./libs/fileSystemService.js');
 const edsParser = require('./libs/edsParser.js');
-const fileManComms = require('./libs/filemanComms.js');
+const filemanComms = require('./libs/filemanComms.js');
 
 module.exports = {   
-   redisService, fsService, edsParser, fileManComms
+   redisService, fsService, edsParser, filemanComms
 };
